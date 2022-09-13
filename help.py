5c619dc88c0ea1e8295a3930d50add022a35a779
xPaulFTW <lascupg13@gmail.com>
2022-09-13 21:54:56 +0300

    Update help.py

--- help.py
+++ help.py
@@ -5,11 +5,11 @@ async def cmd(ctx):
     try:
         channel = await ctx.author.create_dm()
 
-        await channel.send("Command prefix: '$'\n\
-                            $ping <ip address / dns>\n\
-                            $clear <num of mesagess>\n\
-                            $kick <user id>\n\
-                            $ban <user id>")
+        await channel.send("```Command prefix: '$'\n\n\
+    $ping <ip address / dns>\n\
+    $clear <num of mesagess>\n\
+    $kick <user id>\n\
+    $ban <user id>```")
         
     except Exception as err:
         print(err)
