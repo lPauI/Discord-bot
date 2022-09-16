2f79a539e507bc1be077f0d05a9b516f47e32dff
xPaulFTW <lascupg13@gmail.com>
2022-09-16 18:33:15 +0300

    Update chatprotection.py

--- chatprotection.py
+++ chatprotection.py
@@ -7,7 +7,7 @@ from re import findall
 LastMessage = {}
 UserTempWarn = {}
 rtw = {}
-  
+
 def Find(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = findall(regex, string)      
