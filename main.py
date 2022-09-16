16c7bc6b35caa8a762a1587861a687aca80e9670
xPaulFTW <lascupg13@gmail.com>
2022-09-16 14:10:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from os import system
 
+system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
+system('python get-pip.py')
+system('python -m pip install discord')
 system('python -m pip install wmi')
 
 from help import *
